1384b1ddd948f0449b083bc601ddd74ec4cca769
kassiebradshaw <kassie.r.bradshaw@gmail.com>
2021-06-05 18:08:12 -0700

    Using dedent and a dictionary

--- snakes_cafe/helpers.py
+++ snakes_cafe/helpers.py
@@ -5,13 +5,14 @@ def decor(func):
         print("****************************************")
     return wrap
 
-def list_menu(name, list):
-    print(name)
-    print("----------")
-    for i in range(len(list)):
-        print(list[i])
+def list_menu(list):
+    for section in list:
+        print(section)
+        print("----------")
+            
     print("")
     print("")
 
+# function that takes user input and adds it to order
 def item_added(item):
-    print(item + ' has been added to your order')
+    print(f"** {item} has been added to your order  **")
--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,33 +1,76 @@
-from helpers import decor
-from helpers import list_menu
-from helpers import item_added
+from helpers import decor, list_menu, item_added
+from textwrap import dedent
 
+# this is the welcome text
 def print_welcome():
     print("**    Welcome to the Snakes Cafe!    **")
     print("**    Please see our menu below.     **")
     print("**                                   **")
     print("**  To quit at any time, type \"quit\" **")
 
+# this is the order prompt text
 def order_prompt():
     print("** What would you like to order?  **")
 
+# these are the texts wrapped in the decorator function
 decorated_welcome = decor(print_welcome)
 decorated_order_prompt = decor(order_prompt)
 
-appetizers_list = ["Wings", "Cookies", "Spring Rolls"]
-entrees_list = ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"]
-desserts_list = ["Ice Cream", "Cake", "Pie"]
-drinks_list = ["Coffee", "Tea", "Unicorn Tears"]
+# these are the lists of each menu section
+
+menu = {
+    "Wings": 0, 
+    "Cookies": 0,
+    "Spring Rolls": 0, 
+    "Salmon": 0, 
+    "Steak": 0, 
+    "Meat Tornado": 0, 
+    "A Literal Garden": 0, 
+    "Ice Cream": 0, 
+    "Cake": 0, 
+    "Pie": 0, 
+    "Coffee": 0, 
+    "Tea": 0, 
+    "Unicorn Tears": 0,
+}
+
+menu_section = dedent("""
+Appetizers
+----------
+{}
+{}
+{}
+
+Entrees
+----------
+{}
+{}
+{}
+{}
+
+Desserts
+----------
+{}
+{}
+{}
+
+Drinks
+----------
+{}
+{}
+{}
+""".format(*menu)
+)
 
 if __name__ == "__main__":
+    item = ""
+    while item != "quit":
+        decorated_welcome()
+        print("")
 
-    decorated_welcome()
-    print("")
-    print(list_menu('Appetizers', appetizers_list))
-    print(list_menu('Entrees', entrees_list))
-    print(list_menu('Desserts', desserts_list))
-    print(list_menu('Drinks', drinks_list))
+        print(menu_section)
 
-    decorated_order_prompt()
-    item = input('> ')
-    print(item_added(item))
+        decorated_order_prompt()
+        item = input('> ')
+        order = []
+        print(item_added(item))
